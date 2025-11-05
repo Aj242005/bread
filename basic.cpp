6eb82a08bbdc6253c21e8e8c133e103df260a96a
Aj242005 <akshitjain6661@gmail.com>
2025-11-05 17:28:42 +0530

    setup-commit : basic files have been added to just set the repository up

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+	cout<<"Namaste Duniya"<<endl;
+	cout<<"Hello from vim"<<endl;
+	
+
+	return 0;
+}
---
+++ constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    vector<int> keywords = { 
+        'int', // 32 bit integer
+        'float', // 32 bit decimanl number 
+        'double' //64 bit decimal number
+    };
+
+}
