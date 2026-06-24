d9e8417ecbfa18dd8602c332a8a5dbc24e66e821
Aj242005 <akshitjain6661@gmail.com>
2026-06-25 01:07:42 +0530

    made some changes to the token dict

--- bread-utilities.h
+++ bread-utilities.h
@@ -6,76 +6,38 @@
 using namespace std;
 
 
-vector<string> getKeywords(){
-    return keywords;
-}
-
-vector<string> getUnaryOperators(){
-    return unary_operators;
-}
-
-vector<string> getBinaryOperators(){
-    return binary_operators;
+unordered_map<string, string> getTokens(){
+    return tokens;
 }
 
 string getTerminator(){
     return terminator;
 }
 
-bool find(vector<string> standard,string s){
-    for(auto i : standard){
-        if(i == s){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-bool findKeywords(string s){
-    for(auto i : keywords){
-        if(i == s){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-bool findUnaryOperator(string s){
-    for(auto i : unary_operators){
-        if(i == s){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-bool findBinaryOperator(string s){
-    for(auto i : binary_operators){
-        if(i == s){
-            return 1;
-        }
-    }
-    return 0;
+// Does symbol s exist as a token at all?
+bool findToken(string s){
+    return tokens.find(s) != tokens.end();
 }
 
 bool findTerminator(string s){
-    if( s == terminator){
-        return 1;
-    }
-    return 0;
+    return s == terminator;
 }
 
+string getTokenName(string s){
+    auto it = tokens.find(s);
+    return (it != tokens.end()) ? it->second : "unknown";
+}
 
-vector<std::string> removeSpacesFromInput(string s) {
-    vector<std::string> noSpace;
+vector<string> removeSpacesFromInput(string s) {
+    vector<string> noSpace;
     stringstream ss(s);
     string buffer;
-    
-    while (ss >> buffer) { 
+
+    while (ss >> buffer) {
         noSpace.push_back(buffer);
     }
-    
+
     return noSpace;
-} 
+}
 
 #endif
--- standard-constants-main.h
+++ standard-constants-main.h
@@ -2,110 +2,115 @@
 #define STANDARD_CONSTANTS_MAIN_H
 
 #include "dependencies.h"
-
+#include <unordered_map>
 using namespace std;
 
-const vector<string> keywords = { 
-    "int", // 32 bit integer
-    "float", // 32 bit decimanl number 
-    "double", //64 bit decimal number
-    "bool", // boolean type variable
-    "char", // individual character
-    "String", // the standard sequence of characters
-    "Large", // long for c++
-    "if", // conditional statements if
-    "else", // conditional statements else
-    "elseIf", // conditional statements else if and also popularly used as elif in language like python
-    "inline",
-    "switch",
-    "case",
-    "default",
-    "for",
-    "while",
-    "do",
-    "break",
-    "continue",
-    "new",
-    "true",
-    "false",
-    "class",
-    "group",
-    "public",
-    "private",
-    "protected",
-    "try",
-    "catch",
-    "except",
-    "const", // for constant values of a variable but also allows mutable values of the containers
-    "rigid", // this also assignes the constant values to the variables but this doesn't allow the containers to be mutable
-    "return",
-    "auto",
-    "and",
-    "or",
-    "xor",
-    "sizeOf",
-    "extend",
-    "undenfinied",
-    "null",
-    "about_author"
+const unordered_map<string, string> tokens = {
+
+    // ---- keywords ----
+    {"int", "32 bit integer"},
+    {"float", "32 bit decimal number"},
+    {"double", "64 bit decimal number"},
+    {"bool", "boolean type variable"},
+    {"char", "individual character"},
+    {"String", "sequence of characters"},
+    {"Large", "long integer type"},
+    {"if", "conditional if"},
+    {"else", "conditional else"},
+    {"elseIf", "conditional else if"},
+    {"inline", "inline specifier"},
+    {"switch", "switch statement"},
+    {"case", "switch case label"},
+    {"default", "default case label"},
+    {"for", "for loop"},
+    {"while", "while loop"},
+    {"do", "do-while loop"},
+    {"break", "break statement"},
+    {"continue", "continue statement"},
+    {"new", "object instantiation"},
+    {"true", "boolean true literal"},
+    {"false", "boolean false literal"},
+    {"class", "class declaration"},
+    {"group", "grouping construct"},
+    {"public", "public access specifier"},
+    {"private", "private access specifier"},
+    {"protected", "protected access specifier"},
+    {"try", "try block"},
+    {"catch", "catch block"},
+    {"except", "exception block"},
+    {"const", "constant declaration"},
+    {"rigid", "immutable constant declaration"},
+    {"return", "return statement"},
+    {"auto", "automatic type inference"},
+    {"and", "logical and keyword"},
+    {"or", "logical or keyword"},
+    {"xor", "logical xor keyword"},
+    {"sizeOf", "size of operator"},
+    {"extend", "inheritance keyword"},
+    {"undenfinied", "undefined value"},
+    {"null", "null value"},
+    {"about_author", "author metadata keyword"},
+
+    // ---- unary operators ----
+    {"--", "decrement"},
+    {"++", "increment"},
+    {"!", "logical not"},
+    {"~", "bitwise not"},
+    {"$", "set bit counter"},
+    {"ascii", "ascii conversion"},
+    {"anti_ascii", "anti ascii conversion"},
+
+    // ---- binary operators ----
+    {"+", "plus"},
+    {"-", "minus"},
+    {"*", "multiply"},
+    {"/", "divide"},
+    {"//", "integer divide"},
+    {"%", "modulus"},
+    {"==", "equal to"},
+    {"!=", "not equal to"},
+    {"<", "less than"},
+    {">", "greater than"},
+    {"<=", "less than equal to"},
+    {">=", "greater than equal to"},
+    {"&&", "logical and"},
+    {"||", "logical or"},
+    {"&", "bitwise and"},
+    {"|", "bitwise or"},
+    {"^", "bitwise xor"},
+    {">>", "right shift"},
+    {"<<", "left shift"},
+    {"=", "assignment"},
+    {"+=", "plus equal"},
+    {"-=", "minus equal"},
+    {"*=", "multiply equal"},
+    {"/=", "divide equal"},
+    {"%=", "modulus equal"},
+    {"&=", "bitwise and equal"},
+    {"|=", "bitwise or equal"},
+    {"^=", "bitwise xor equal"},
+    {"<<=", "left shift equal"},
+    {">>=", "right shift equal"},
+    {"?", "ternary question"},
+    {":", "ternary colon"},
+    {".", "dot member access"},
+    {"->", "arrow member access"},
+    {"(", "open parenthesis"},
+    {")", "close parenthesis"},
+    {"()", "empty parenthesis"},
+    {"{}", "empty braces"},
+    {"{", "open brace"},
+    {"}", "close brace"},
+    {"[", "open bracket"},
+    {"]", "close bracket"},
+    {"[]", "empty bracket"},
+
+    // ---- terminator ----
+    {">~", "statement terminator"}
 };
 
-const vector<string> unary_operators = {
-    "--",
-    "++",
-    "!",
-    "~",
-    "$", // find the set bits in the number
-    "ascii",
-    "anti_ascii"
-};
-
-const vector<string> binary_operators = {
-    "+",
-    "-",
-    "*",
-    "/",
-    "//",
-    "%",
-    "==",
-    "!=",
-    "<",
-    ">",
-    "<=",
-    ">=",
-    "&&",
-    "||",
-    "&",
-    "|",
-    "^",
-    ">>",
-    "<<",
-    "=",
-    "+=",
-    "-=",
-    "*=",
-    "/=",
-    "%=",
-    "&=",
-    "|=",
-    "^=",
-    "<<=",
-    ">>=",
-    "?",
-    ":",
-    ".",
-    "->",
-    "(",
-    ")",
-    "()",
-    "{}",
-    "{",
-    "}",
-    "[",
-    "]",
-    "[]"
-};
-
+// Kept as a plain constant too, since end-of-statement checks are common
+// enough to want a direct comparison instead of a map lookup.
 const string terminator = ">~";
 
 #endif
